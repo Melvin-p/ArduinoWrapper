c2e16ef4c7d25f060fe9d8c1b439c2cf1144474b
Melvin <70172420+Melvin-p@users.noreply.github.com>
2023-01-09 22:25:12 +0000

    tweaks to the build system

--- src/library/ArduinoWrapper/adwr.hpp
+++ src/library/ArduinoWrapper/adwr.hpp
@@ -9,13 +9,13 @@
 #include "./time.hpp"
 
 #ifndef __def_main__
-	#include "def.hpp"
+#include "def.hpp"
 #endif
 
 #ifndef __custom__string__
-	#include "./library/string.hpp"
+#include "./library/string.hpp"
 #endif
 
-#ifndef __lcd_ipc__
-	#include "Adafruit_RGBLCDShield.hpp"
+#ifdef lcd_enabled
+#include "Adafruit_RGBLCDShield.hpp"
 #endif
