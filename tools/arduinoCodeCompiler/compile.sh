0de0acab4b27626cd42c84af81159cd028ab791b
Melvin <70172420+Melvin-p@users.noreply.github.com>
2023-01-26 15:12:22 +0000

    compile bash script changed script for checking ino file, and pre processing it now has been merged in with the main compile script output names now depend on the input filename

--- tools/arduinoCodeCompiler/compile.sh
+++ tools/arduinoCodeCompiler/compile.sh
@@ -3,19 +3,28 @@
 <<"COMMENT"
 options are
 -o output dir i.e /foo/bar/temp/
--f input cpp file i.e /for/bar/files/sample.cpp
--p name of output executable i.e. sample
+-f input ino file i.e /for/bar/files/sample.ino
 
 all options are required full normalised path is required
 if there ae space in the path to this file or provided path this script will not work
 
-assuming g++, ar exists
+assuming clang++, clang-format, ar, ardunio-cli exists on path
+
+The following commands need to be run to setup up arduino-cli for first time use
+arduino-cli config init
+arduino-cli core update-index
+arduino-cli core install arduino:avr
+arduino-cli lib install "Adafruit RGB LCD Shield Library"
+
 assuming build outputs in build/Release/output folder
 assuming inputted paths are normalized and valid
+assuming output directory exists
+assuming input file exists
+assuming sed supports -E flag
 COMMENT
 
 
-while getopts 'o:f:p:' options 
+while getopts 'o:f:' options 
 do
     case $options in
         o)
@@ -26,10 +35,6 @@ do
         IN_FILE=$OPTARG
         export IN_FILE
         ;;
-        p)
-        OUT_NAME=$OPTARG
-        export OUT_NAME
-        ;;
         *)
         exit 1
         ;;
@@ -37,7 +42,7 @@ do
 done
 
 
-if [ -z "${OUT_DIR}" ] || [ -z "${IN_FILE}" ] || [ -z "${OUT_NAME}" ]; then 
+if [ -z "${OUT_DIR}" ] || [ -z "${IN_FILE}" ] ; then 
     exit 1;
 fi
 
@@ -45,33 +50,63 @@ fi
 # supporting files for compliation are assumed to be in the same dir as script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# build exectuable
+# check ino file is compilable normally exit if not
 
-c++ -DLINUX  -O3 -DNDEBUG -include "$SCRIPT_DIR/sample_private.hpp" -I "$SCRIPT_DIR/../../src/library/ArduinoWrapper/" -std=gnu++17 -o "$OUT_DIR/$OUT_NAME.o" -c "$IN_FILE"
+arduino-cli compile --fqbn arduino:avr:uno "$IN_FILE"
 
 if [ $? -ne 0 ]; then
-    exit 1;
+    exit 167;
 fi
 
-ar rcs "$OUT_DIR/$OUT_NAME.a" "$OUT_DIR/$OUT_NAME.o"
+# copy the file over and change the extension
+# run clang format this will make string hacking to remove includes easier
+
+filename=$(basename -- "$IN_FILE")
+filename_noext="${filename%.*}"
+
+cp "$IN_FILE" "$OUT_DIR/$filename_noext.cpp"
+
+clang-format --files= "$OUT_DIR/$filename_noext.cpp" -i
 
 if [ $? -ne 0 ]; then
-    exit 1;
+    exit 168;
+fi
+
+#remove includes
+
+cat "$OUT_DIR/$filename_noext.cpp" | sed -E "s|^(#\s*include\s+[<\"]\S*[>\"])\s*((//.*)?(/\**.*)?)$|\2|g" > "$OUT_DIR/${filename_noext}_processed.cpp"
+
+if [ $? -ne 0 ]; then
+    exit 169;
+fi
+
+# build exectuable
+
+c++ -DLINUX  -O3 -DNDEBUG -include "$SCRIPT_DIR/sample_private.hpp" -I "$SCRIPT_DIR/../../src/library/ArduinoWrapper/" -std=gnu++17 -o "$OUT_DIR/$filename_noext.o" -c "$OUT_DIR/${filename_noext}_processed.cpp"
+
+if [ $? -ne 0 ]; then
+    exit 170;
+fi
+
+ar rcs "$OUT_DIR/$filename_noext.a" "$OUT_DIR/$filename_noext.o"
+
+if [ $? -ne 0 ]; then
+    exit 171;
 fi
 
 c++ -DLINUX  -O3 -DNDEBUG -std=gnu++17 -o "$OUT_DIR/main.cpp.o" -c "$SCRIPT_DIR/main.cpp"
 
 if [ $? -ne 0 ]; then
-    exit 1;
+    exit 172;
 fi
 
-c++ -o  "$OUT_DIR/$OUT_NAME.out" "$OUT_DIR/main.cpp.o" -l:"$OUT_NAME.a" -l:"libArduinoWrapper.so" -L"$SCRIPT_DIR/../../build/Release/output/" -L"$OUT_DIR"
+c++ -o  "$OUT_DIR/$filename_noext.out" "$OUT_DIR/main.cpp.o" -l:"$filename_noext.a" -l:"libArduinoWrapper.so" -L"$SCRIPT_DIR/../../build/Release/output/" -L"$OUT_DIR"
 
 if [ $? -ne 0 ]; then
-    exit 1;
+    exit 173;
 fi
 
 # run file after appending to library path
-# LD_APPEND=$(realpath $SCRIPT_DIR/../../build/Release/output/)
-# export LD_LIBRARY_PATH=$LD_APPEND:$LD_LIBRARY_PATH
-# $OUT_DIR/$OUT_NAME.out
+#LD_APPEND=$(realpath $SCRIPT_DIR/../../build/Release/output/)
+#export LD_LIBRARY_PATH=$LD_APPEND:$LD_LIBRARY_PATH
+#$OUT_DIR/$filename_noext.out
