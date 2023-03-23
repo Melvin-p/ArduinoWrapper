b734cb85d2ee4aca1c2258bc8a19213459851c45
Melvin <70172420+Melvin-p@users.noreply.github.com>
2023-03-23 23:20:27 +0000

    progress on automated testing

--- tools/automated-testing.py
+++ tools/automated-testing.py
@@ -11,6 +11,9 @@ import platform
 import subprocess
 import glob
 import uuid
+from concurrent.futures.thread import ThreadPoolExecutor
+import time
+import signal
 
 if platform.system() != "Linux":
     print("Linux not detected", file=sys.stderr)
@@ -19,7 +22,6 @@ if platform.system() != "Linux":
 # make porting in the future easier
 path_sep: str = os.pathsep
 sep: str = os.sep
-
 adwr_name: str = "libArduinoWrapper.so"
 arduino_test_name: str = "arduinoCodeTest"
 this_path: str = os.path.abspath(os.path.dirname(__file__))
@@ -28,21 +30,6 @@ compile_script_path: str = this_path + sep + "arduinoCodeCompiler" + sep + "comp
 
 def full_path(path: str) -> str:
     return os.path.abspath(path)
-
-
-def compile_and_run(ino_path: str):
-    cwd: str = os.getcwd()
-    out_folder = os.path.dirname(ino_path) + sep
-    os.chdir(out_folder)
-    script_out: subprocess.CompletedProcess = subprocess.run(
-        ["bash", compile_script_path, "-f" + ino_path, "-o" + out_folder], capture_output=True)
-    """ todo 
-    run arduinoCode
-    run arduinoCodeTest
-    run arduinoCode asynchronously 
-    once test finishes kill arduinoCode
-    """
-    os.chdir(cwd)
 
 
 parser = argparse.ArgumentParser(
@@ -96,7 +83,17 @@ if not os.path.isfile(testing_exe):
     print("ArduinoCodeTest executable not found", file=sys.stderr)
     exit(1)
 
+try:
+    subprocess.run(["arduino-cli", "version"])
+except subprocess.CalledProcessError:
+    print("arduino-cli not found", file=sys.stderr)
+    exit(1)
+
 os.environ["PATH"] += path_sep + bin_dir
+if "LD_LIBRARY_PATH" in os.environ:
+    os.environ["LD_LIBRARY_PATH"] += path_sep + bin_dir
+else:
+    os.environ["LD_LIBRARY_PATH"] = bin_dir
 
 if self_test:
     adwr_test: str = os.path.join(bin_dir, "ArduinoWrapper-test")
@@ -118,7 +115,56 @@ for item in os.listdir(input_dir):
         for file in glob.glob(temp_dir + sep + "*ino"):
             ino_paths.append(file)
 
-compile_and_run(ino_paths[1])
-
+uuids: list[str] = []
 for i in ino_paths:
-    print(uuid.uuid4())
+    uuids.append(str(uuid.uuid4()))
+
+jobs: list[tuple[str, str]] = list(zip(ino_paths, uuids))
+
+
+def compile_and_run(job: tuple[str, str]) -> tuple[bool, str]:
+    work_dir: str = os.path.dirname(job[0]) + sep
+    filename: str = os.path.basename(job[0])
+    filename_noext: str = filename.replace(".ino", "")
+    try:
+        os.mkdir(work_dir + sep + "output")
+    except FileExistsError:
+        pass
+    except FileNotFoundError:
+        print("missing folder cannot continue", file=sys.stderr)
+        return False, job[0]
+    output_dir: str = os.path.join(work_dir, "output") + sep
+    log = open(file=output_dir + "automated-testing.log", mode="a", encoding="utf-8", errors='ignore', newline="\n")
+    script_out = subprocess.run(["bash", compile_script_path, "-f" + job[0], "-o" + output_dir], capture_output=False)
+    match script_out.returncode:
+        case 0:
+            log.write("file compiles for arduino\n")
+            log.write("file compiled for native\n")
+        case 167:
+            log.write("failed to compile ino for arduino\n")
+        case 168:
+            log.write("clang format failed\n")
+        case 169:
+            log.write("file preprocessing failed\n")
+        case 170:
+            log.write("processed file failed to compile\n")
+        case _:
+            log.write("a compile error occurred\n")
+    if script_out.returncode == 0:
+        arduinoCode = subprocess.Popen([output_dir + sep + filename_noext + ".out"],
+                                       env=dict(os.environ, **{"ard_ipc": job[1]}))
+        time.sleep(60)
+        arduinoCode.send_signal(signal.SIGTERM)
+        arduinoCode.wait()
+    """ todo 
+    run arduinoCodeTest
+    run arduinoCode asynchronously 
+    once test finishes kill arduinoCode
+    """
+    log.close()
+    return True, job[0]
+
+
+compile_and_run(jobs[1])
+
+executor = ThreadPoolExecutor(max_workers=threads)
