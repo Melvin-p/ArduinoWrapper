323cad0670a8234cfbd3bcd2a32a32cae8edb014
Melvin <70172420+Melvin-p@users.noreply.github.com>
2023-03-25 13:39:56 +0000

    pull request  #32 prevent hang

--- miscellaneous/arduinoCode/main.cpp
+++ miscellaneous/arduinoCode/main.cpp
@@ -1,10 +1,14 @@
+#include <SerialIPC.hpp>
 #include <csignal>
 
 #include "sample.hpp"
 bool run = true;
 
+SerialIPC &serialipc = SerialIPC::getInstance();
+
 void sigint_handler(int sig) {
     run = false;
+    serialipc.setBlocked(false);
 }
 
 int main(int argc, char *argv[]) {
